77a7853d182bcc10f000f0492d05624f017101e9
meixuexiang <meixuexiang@16c5a628-9387-11de-b51d-eb253e4b719b>
2009-10-08 09:16:10 +0000

    只允许单实例打开

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 CheckUpdate({
-	version:'V3.0.6',
-	filename:"hosteditorV3.0.6.hta",
-	content:'修正了检查更新状态无法保存的bug\n修正了用记事本打开格式不正常的bug\n修正了tips里会好多换行的bug'
+	version:'V3.0.7',
+	filename:"hosteditorV3.0.7.hta",
+	content:'添加单实例打开功能，现在只能同时存在一个程序窗口'
 })
