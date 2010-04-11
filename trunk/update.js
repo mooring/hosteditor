c6fd1a4795a68a3c9e7c016de34e1a326c1c3ea0
meixuexiang <meixuexiang@16c5a628-9387-11de-b51d-eb253e4b719b>
2010-04-11 10:51:38 +0000

    V3.2发布

--- trunk/update.js
+++ trunk/update.js
@@ -1,5 +1,5 @@
 CheckUpdate({
-	version:3.1,
-	filename:"hosteditorV3.1.hta",
-	content:'修正添加不保存的bug，\n修正如果添加的规则已经存在时，选中状态不跟随新添加的bug'
+	version:3.2,
+	filename:"hosteditorV3.2.hta",
+	content:'使用最新的JQuery1.4.2，运行速度更快，\n对界面进行了优化调整，\n修正了添加多条规则时的bug，\n可以在图形模式调整规则的顺序，\n增加以新进程打开IE功能，\n等'
 });
